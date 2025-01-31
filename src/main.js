95a93f74bdc27e744ef08c6cc392192896ca90aa
Blasivas <matheus.quintalves@gmail.com>
2025-01-31 20:23:30 -0300

    Aplicação rodando no servidor

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use extrict"
+
+import "./styles/global.css"
+import "./styles/card.css"
+import "./styles/form.css"
+import "./styles/header.css"
+import "./styles/profile.css"
+
+import "./modules/submit.js"
